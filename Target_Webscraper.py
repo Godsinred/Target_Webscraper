4e8dbb7324c4d6e356e959b2ca9b85c453ac6022
Jonathan Ishii <you@example.comjtishii@yahoo.com>
2018-01-26 01:42:51 -0800

    Added the ability to scrape all categories and hyperlinks, working on getting all items now. Have it running just need to test print.

--- Target_Webscraper.py
+++ Target_Webscraper.py
@@ -19,6 +19,22 @@ from selenium.common.exceptions import TimeoutException
 # how to install chromedriver
 # https://www.youtube.com/watch?v=dz59GsdvUF8
 
+def print_category_links(category_links):
+    """
+    Prints all the category and its links
+    :param category_links: a dictionary of dictionary of the categories and the links
+    :return: nothing
+    """
+
+    for main_cat, sub_cat in category_links.items():
+        print(main_cat)
+        #sub cat name and hypterlink
+        for key, value in sub_cat.items():
+            print(key + ": " + value)
+
+def print_item_list(item_list):
+    
+
 def get_sub_categories(hyperlink):
     """
     The purpose of this is to get all the sub categories and its corresponding links
@@ -26,8 +42,15 @@ def get_sub_categories(hyperlink):
     :return: a dictionary of sub category names and its hyperlinks as its value
     """
 
+    # configure chrome options
+    options = webdriver.ChromeOptions()
+    # tells the browser to be headless
+    options.add_argument("headless")
+    # set the window size
+    options.add_argument('window-size=1200x600')
+
     # initiates the webdriver
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(chrome_options=options)
     # opens the browser
     driver.get(hyperlink)
 
@@ -36,15 +59,42 @@ def get_sub_categories(hyperlink):
     driver.implicitly_wait(DELAY)
     wait = WebDriverWait(driver, DELAY)
 
-    wait.until(EC.visibility_of_all_elements_located((By.XPATH, """//*[@id="js-plp-page-title"]""")))
-    wait.until(EC.visibility_of_all_elements_located((By.XPATH, """// *[ @ id = "main"]""")))
-    wait.until(EC.visibility_of_all_elements_located((By.XPATH, """// *[ @ id = "home"]""")))
+    #time.sleep(10)
+
+    # try:
+    #     wait.until(EC.element_to_be_clickable((By.XPATH, """//*[@id="header"]/nav[1]/a[1]""")))
+    # except:
+    #     print("No return home page button.")
+    #
+    # try:
+    #     wait.until(EC.element_to_be_clickable((By.XPATH, """//*[@id="js-toggleLeftNav"]""")))
+    # except:
+    #     print("No navigation page button.")
+    #
+    # try:
+    #     wait.until(EC.element_to_be_clickable((By.XPATH, """//*[@id="js-toggleLeftNav"]""")))
+    # except:
+    #     print("No browseByCategory.")
+    #
+    # try:
+    #     wait.until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "categories")))
+    # except:
+    #     print("No categories.")
+
 
     # gets all the sub categories and its hyperlinks
     try:
         try:
-            title = driver.find_element_by_xpath("""//*[@id="js-plp-page-title"]""")
-            print(title.text)
+            # wait.until(EC.text_to_be_present_in_element((By.XPATH, """//*[@id="js-plp-page-title"]""")))
+            # title = driver.find_element_by_xpath("""//*[@id="js-plp-page-title"]""")
+            # print(title.text)
+
+            wait.until(EC.element_to_be_clickable((By.XPATH, """//*[@id="header"]/nav[1]/a[1]""")))
+            wait.until(EC.element_to_be_clickable((By.XPATH, """//*[@id="js-toggleLeftNav"]""")))
+            wait.until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "js-browseByCategory")))
+
+            wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "categories")))
+            wait.until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "categories")))
 
             category_list = driver.find_elements_by_class_name("categories")
 
@@ -69,7 +119,59 @@ def get_sub_categories(hyperlink):
 
     return sub_categories
 
+def get_items(main_cat, key, hyperlink, item_list):
+    '''
+
+    :param main_cat: the main category to be saved
+    :param key: the sub category to be saved
+    :param hyperlink: the hyperlink of the subcategory to get all the items from
+    :param item_list: all the items that target has on their site
+    :return:
+    '''
+    # configure chrome options
+    options = webdriver.ChromeOptions()
+    # tells the browser to be headless
+    options.add_argument("headless")
+    # set the window size
+    options.add_argument('window-size=1200x600')
+
+    # initiates the webdriver
+    driver = webdriver.Chrome(chrome_options=options)
+    # opens the browser
+    driver.get(hyperlink)
+
+    # setting up the waits
+    DELAY = 10
+    driver.implicitly_wait(DELAY)
+    wait = WebDriverWait(driver, DELAY)
+
+    # CODE HERE <<<<<<<<<<<<<<<<------------------------------------
+    wait.until(EC.element_to_be_clickable(By.XPATH, """//*[@id="plp"]/section/div[2]/div/ul"""))
+    wait.until(EC.visibility_of_all_elements_located(By.CLASS_NAME, "product"))
+
+    items = driver.find_element_by_xpath("""//*[@id="plp"]/section/div[2]/div/ul""")
+
+    for item in items:
+        # data-ids are 8digit numbers
+        id = item.get_attribute("data-id")
+        # could be a range of prices or on sale
+        price = item.find_element_by_class_name("price").text
+        # title of the item
+        name = item.find_element_by_class_name("truncated-title").text
+
+        # dictionary -> of list of list with float a
+        item_list[id] = [[main_cat, key], name, price]
+
+
 def main():
+
+    # # configure chrome options
+    # options = webdriver.ChromeOptions()
+    # # tells the browser to be headless
+    # options.add_argument("headless")
+    # # set the window size
+    # options.add_argument("window-size=1200x600")
+    # driver = webdriver.Chrome(chrome_options=options)
 
     # created selenium object
     driver = webdriver.Chrome()  # replace with .Firefox(), or with the browser of your choice
@@ -84,7 +186,9 @@ def main():
     # navigate to the page
     driver.get(url)
 
+    category_links = {}
     try:
+        # driver.save_screenshot("screenshot.png")
         # opens up the side bar javascript
         wait.until(EC.element_to_be_clickable((By.XPATH, """//*[@id="js-toggleLeftNav"]""")))
         button = driver.find_element_by_xpath("""//*[@id="js-toggleLeftNav"]""")
@@ -99,25 +203,35 @@ def main():
         wait.until(EC.element_to_be_clickable((By.XPATH, """//*[@id="leftNavigation"]/ul[2]""")))
         items = driver.find_element_by_xpath("""//*[@id="leftNavigation"]/ul[2]""").find_elements_by_tag_name("li")
 
-
         # gets the hyperlink and category name but the first and the last,
         # since the first is back to main menu and the last is exit
-        category_links = {}
+
         for i in range(1, len(items) - 1):
             hyperlink = items[i].find_element_by_tag_name('a').get_attribute('href')
             category_name = items[i].text
-            sub_categories = get_sub_categories(hyperlink)
-            # makes a dictionary of dictionary
-            # main category being the first key and the second dictionary will have a key of sub category name
-            #  with the hyperlink to it
-            category_links[category_name] = sub_categories
+            if(category_name != "Target Finds"):
+                sub_categories = get_sub_categories(hyperlink)
+                # makes a dictionary of dictionary
+                # main category being the first key and the second dictionary will have a key of sub category name
+                #  with the hyperlink to it
+                category_links[category_name] = sub_categories
 
-        print(category_links)
+        # print all the categories with the sub cats and hyperlinks
+        print_category_links(category_links)
 
     except TimeoutException:
         print("Loading took too long.")
 
-    time.sleep(30)
+    finally:
+        driver.quit()
+
+    # gets all the items from targets site
+    item_list = {}
+    for main_cat, sub_cat in category_links.items():
+        for key, value in sub_cat.items():
+            get_items(main_cat, key, value, item_list)
+
+    #
 
 if __name__ == "__main__":
     main()
